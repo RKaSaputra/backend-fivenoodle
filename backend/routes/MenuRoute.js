b54f5286eb9067cfb2c7b0af7e323e9fe00c2d5d
prabuhadi <prabuhadiyo@gmail.com>
2023-02-21 16:27:04 +0700

    update and delete

--- backend/routes/MenuRoute.js
+++ backend/routes/MenuRoute.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { getMenus } from "../controllers/MenuController.js";
+import {
+  getMenus,
+  getMenusById,
+  createMenus,
+  updateMenus,
+  deleteMenus,
+} from "../controllers/MenuController.js";
 
 const router = express.Router();
 
 router.get("/menu", getMenus);
+router.get("/menu/:id", getMenusById);
+router.post("/menu", createMenus);
+router.patch("/menu/:id", updateMenus);
+router.delete("/menu/:id", deleteMenus);
 
 export default router;
