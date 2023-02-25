e0d610c13b5c378e9b6c55619a7a962b1433a020
Dika Saputra <69504128+RKaSaputra@users.noreply.github.com>
2023-02-25 21:05:26 +0700

    User n Menu

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,22 @@
 import express from "express";
 import cors from "cors";
-<<<<<<< HEAD
 import session from "express-session";
 import SequelizeStore from "connect-session-sequelize";
 import dotenv from "dotenv";
 import db from "./config/Database.js";
 import UserRoute from "./routes/UserRoute.js";
 import AuthRoute from "./routes/AuthRoute.js";
-dotenv.config();
+import MenuRoute from "./routes/MenuRoute.js";
+import MenuCategoryRoute from "./routes/MenuCategoryRoute.js";
 
+dotenv.config();
 const app = express();
 
 const sessionStore = SequelizeStore(session.Store);
 const store = new sessionStore({
   db: db,
 });
+
 // (async () => {
 //   await db.sync();
 // })();
@@ -36,20 +38,13 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+
 app.use(express.json());
+app.use(cors());
 app.use(UserRoute);
 app.use(AuthRoute);
-
-// store.sync();
-=======
-import MenuRoute from "./routes/MenuRoute.js";
-import MenuCategoryRoute from "./routes/MenuCategoryRoute.js";
-
-const app = express();
-app.use(cors());
-app.use(express.json());
 app.use(MenuRoute);
 app.use(MenuCategoryRoute);
->>>>>>> prabu
 
 app.listen(5000, () => console.log("Server up and Running"));
+// store.sync();
