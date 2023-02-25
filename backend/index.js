c47e4ed459b18da8e81884284db578fb6e8ef4d4
Dika Saputra <69504128+RKaSaputra@users.noreply.github.com>
2023-02-25 20:22:12 +0700

    backup

--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,21 @@
-// import express from "express";
-// import cors from "cors";
-// import UserRoute from "./routes/UserRoute.js";
-// import MenuRoute from "./routes/MenuRoute.js";
-// import MenuCategoryRoute from "./routes/MenuCategoryRoute.js";
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-// app.use(UserRoute);
-// app.use(MenuRoute);
-// app.use(MenuCategoryRoute);
-
-// app.listen(5000, () => console.log("Server up and Running"));
-
 import express from "express";
 import cors from "cors";
-<<<<<<< HEAD
 import session from "express-session";
 import SequelizeStore from "connect-session-sequelize";
+import FileUpload from "express-fileupload";
 import dotenv from "dotenv";
 import db from "./config/Database.js";
-import UserRoute from "./routes/UserRoute.js";
-import AuthRoute from "./routes/AuthRoute.js";
-dotenv.config();
-
-=======
-import FileUpload from "express-fileupload";
-// import UserRoute from "./routes/UserRoute.js";
 import BlogRoute from "./routes/BlogRoute.js";
 import BlogCategoryRoute from "./routes/BlogCategoryRoute.js";
->>>>>>> Alphard
+
+dotenv.config();
+
 const app = express();
 
 const sessionStore = SequelizeStore(session.Store);
 const store = new sessionStore({
   db: db,
 });
-// (async () => {
-//   await db.sync();
-// })();
 
 app.use(
   session({
@@ -57,18 +34,13 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
-app.use(express.json());
-<<<<<<< HEAD
-app.use(UserRoute);
-app.use(AuthRoute);
 
-// store.sync();
-=======
+app.use(express.json());
 app.use(FileUpload());
 app.use(express.static("public"));
-// app.use(UserRoute);
 app.use(BlogCategoryRoute);
 app.use(BlogRoute);
->>>>>>> Alphard
+
+// store.sync();
 
 app.listen(process.env.APP_PORT, () => console.log("Server up and Running"));
--- backend/models/BlogCategoryModel.js
+++ backend/models/BlogCategoryModel.js
@@ -3,51 +3,14 @@ import db from "../config/Database.js";
 
 const { DataTypes } = Sequelize;
 
-<<<<<<< HEAD:backend/models/UserModel.js
-const Users = db.define(
-  "users",
-  {
-    uuid: {
-      type: DataTypes.STRING,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-        len: [3, 15],
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-=======
 const BlogCategory = db.define(
   "blog_categories",
   {
     Nama: DataTypes.STRING,
->>>>>>> Alphard:backend/models/BlogCategoryModel.js
   },
   {
     freezeTableName: true,
   }
 );
 
-<<<<<<< HEAD:backend/models/UserModel.js
-export default Users;
-=======
 export default BlogCategory;
-
-// (async () => {
-//   await db.sync();
-// })();
->>>>>>> Alphard:backend/models/BlogCategoryModel.js
